0de65e3075c28e0c0c3e9c2e46f18ae0708022e8
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2021-08-02 19:10:19 -0400

    Remove unnecessary logs

--- yupiwrap/__init__.py
+++ yupiwrap/__init__.py
@@ -10,12 +10,10 @@ def yupi2traja(yupi_trajectory):
         x = yupi_trajectory.r.x
         y = yupi_trajectory.r.y
         t = yupi_trajectory.t
-        print(x)
     return traja.TrajaDataFrame({"x": x, "y": y, "time": t})
 
 def traja2yupi(traja_trajectory):
     x = np.array(traja_trajectory.x)
     y = np.array(traja_trajectory.y)
     t = np.array(traja_trajectory.time)
-    print(x)
     return yupi.Trajectory(x=x, y=y, t=t)
